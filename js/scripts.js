d3e9ec99c20a4a2460a42a3950e5631bac5e0593
Elly Maimon <eliranmaimon@gmail.com>
2018-06-15 09:34:14 -0700

    add prototypes for toppings and price

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
-function Pizza(topping, size, price) {
-  this.topping = topping;
+//Builds a pizza object
+function Pizza(size, price) {
+  this.toppings = [];
   this.size = size;
   this.price = price;
 }
 
-Pizza.prototype.determinePrice = function() {
-  
+//Adds toppings to Pizza
+Pizza.prototype.addToppings = function(usersToppings) {
+  usersToppings.forEach(function(topping){
+    this.toppings.push(topping);
+  });
 }
+
+
+//Determines the price of the Pizza
+Pizza.prototype.determinePrice = function() {
+  var totalToppings = this.toppings.length;
+  var addedCost = totalToppings * 0.75;
+
+}
+
+
+
+$(function(){
+  $(".user-order").submit(function(event){
+    event.preventDefault();
+
+  });
+});
